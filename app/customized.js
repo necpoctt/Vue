00b62046a69f2e147d97f731de43be66eb81e599
howardliao <howard.liao@xuenn.com>
2018-07-20 17:05:32 +0800

    add vue

---
+++ app/customized.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+
+    Vue.component("view-customized", {
+        template: "#view-customized-template",
+        data: function () {
+            return {
+                id: ""
+            };
+        },
+    });
+    var app = new Vue({ el: "#app" });
+})
